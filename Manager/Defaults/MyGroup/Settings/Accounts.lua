c995798ca56e6e615af52466631c711a1d9c9695
int3code <int3code@gmail.com>
2021-09-18 12:58:38 +0200

    disabled era accounts in defaults

--- Manager/Defaults/MyGroup/Settings/Accounts.lua
+++ Manager/Defaults/MyGroup/Settings/Accounts.lua
@@ -5,9 +5,9 @@ local _, ADDON = ...
 local accounts = {
   [0] = "?", -- do not remove!
 --[1] = {basegp = 200, name = "MC"  },
-  [2] = {basegp = 200, name = "BWL" },
-  [3] = {basegp = 200, name = "AQ40"},
-  [4] = {basegp = 200, name = "Naxx"},
+--[2] = {basegp = 200, name = "BWL" },
+--[3] = {basegp = 200, name = "AQ40"},
+--[4] = {basegp = 200, name = "Naxx"},
   [5] = {basegp = 200, name = "SSC/TK"},
 }
 
